f50f324a1faaff20e5a8b24bb2c78f711d73159e
chetas11 <cbirmole@gmail.com>
2020-10-31 20:03:34 +0530

    SignIn button

--- script.js
+++ script.js
@@ -11,25 +11,25 @@
         let Inbox = document.getElementById("inbox")
         MainContainer.classList.add("hide")
         Inbox.classList.remove("hide")
-        // console.log(googleUser.wc.access_token)
+        console.log(googleUser.wc.access_token)
 
 
-        // function authenticate() {
-        //     return gapi.auth2.getAuthInstance()
-        //         .signIn({scope: "https://mail.google.com/ https://www.googleapis.com/auth/gmail.compose https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/gmail.readonly"})
-        //         .then(function() { console.log("Sign-in successful"); },
-        //             function(err) { console.error("Error signing in", err); });
-        // }
-        // function loadClient() {
-        //     gapi.client.setApiKey("YOUR_API_KEY");
-        //     return gapi.client.load("https://gmail.googleapis.com/$discovery/rest?version=v1")
-        //         .then(function() { console.log("GAPI client loaded for API"); },
-        //             function(err) { console.error("Error loading GAPI client for API", err); });
-        // }
-        // Make sure the client is loaded and sign-in is complete before calling this method.
+        function authenticate() {
+            return gapi.auth2.getAuthInstance()
+                .signIn({scope: "https://mail.google.com/ https://www.googleapis.com/auth/gmail.compose https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/gmail.readonly"})
+                .then(function() { console.log("Sign-in successful"); },
+                    function(err) { console.error("Error signing in", err); });
+        }
+        function loadClient() {
+            gapi.client.setApiKey("AIzaSyCL3tu8G68zXRgjJJQD55TXsGP6_ztlmVI");
+            return gapi.client.load("https://gmail.googleapis.com/$discovery/rest?version=v1")
+                .then(function() { console.log("GAPI client loaded for API"); },
+                    function(err) { console.error("Error loading GAPI client for API", err); });
+        }
+
         function execute() {
             return gapi.client.gmail.users.getProfile({
-            "userId": "111490570980223679468"
+            "userId": Id
             })
                 .then(function(response) {
                         // Handle the results here (response.result has the parsed body).
