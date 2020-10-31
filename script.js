121b57944772440b0b22d00007952f47c7c8c7b7
chetas11 <cbirmole@gmail.com>
2020-10-31 15:01:03 +0530

    SignIn button

--- script.js
+++ script.js
@@ -19,9 +19,19 @@
                 console.log(e)
             }
         }
-        
+
 
         Fetcher();
+
+        let SignOut = document.getElementById("signOut");
+
+        SignOut.addEventListener("click", ()=>{
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+            console.log('User signed out.');
+        });
+
+        })
         
         
 }
