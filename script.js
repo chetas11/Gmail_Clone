f343c7ff7b71590c684d6ba3915eac9d6b90bc8e
chetas11 <cbirmole@gmail.com>
2020-10-31 21:08:13 +0530

    SignIn button

--- script.js
+++ script.js
@@ -82,6 +82,7 @@
             var auth2 = gapi.auth2.getAuthInstance();
             auth2.signOut().then(function () {
             console.log('User signed out.');
+            location.reload();
         });
 
         })
