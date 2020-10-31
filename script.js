73b2b5d65ae2481bca2f450c66921fbc7707b75d
chetas11 <cbirmole@gmail.com>
2020-10-31 14:32:58 +0530

    SignIn button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
     function onSignIn(googleUser) {
-        // let profile = googleUser.getBasicProfile();
+        let profile = googleUser.getBasicProfile();
         // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         // console.log('Name: ' + profile.getName());
         // console.log('Image URL: ' + profile.getImageUrl());
