3b1e820f8f485f082467258706201076755e766c
chetas11 <cbirmole@gmail.com>
2020-10-31 14:50:31 +0530

    SignIn button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
     function onSignIn(googleUser) {
         let profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        let Id = profile.getId();
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
@@ -9,6 +10,18 @@
         let MainContainer = document.getElementById("main")
         MainContainer.classList.add("hide")
         
+        const Fetcher = async () => {
+            try{
+                const response = await fetch("https://gmail.googleapis.com/gmail/v1/users/"+Id+"/messages")
+                const data = await response.json()
+                console.log(data)
+            }catch(e){
+                console.log(e)
+            }
+        }
+        
+
+        Fetcher();
         
         
 }
