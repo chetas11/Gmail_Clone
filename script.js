5e66118b4f0963af74c8dad6fa1a2a72f0a36ac2
chetas11 <cbirmole@gmail.com>
2020-10-31 20:52:05 +0530

    SignIn button

--- script.js
+++ script.js
@@ -65,10 +65,11 @@
 
         
         gapi.load("client:auth2", function() {
-            gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
+            gapi.auth2.init({client_id: "949258149932-60i8eajtnv5q35cl0amcn7q7qgpl6ihv.apps.googleusercontent.com"});
         });
 
-        authenticate().then(loadClient).then(getMails())
+        authenticate().then(loadClient)
+        setTimeout(getMails(),1000);
         
 
         let SignOut = document.getElementById("signOut");
@@ -78,6 +79,7 @@
             var auth2 = gapi.auth2.getAuthInstance();
             auth2.signOut().then(function () {
             console.log('User signed out.');
+            location.href('index.html')
         });
 
         })
