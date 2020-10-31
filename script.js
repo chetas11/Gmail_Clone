64e9cf7724ea6d2f67c94fe9b64f52d178d4a339
chetas11 <cbirmole@gmail.com>
2020-10-31 14:38:10 +0530

    SignIn button

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
     function onSignIn(googleUser) {
         let profile = googleUser.getBasicProfile();
-        // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        // console.log('Name: ' + profile.getName());
-        // console.log('Image URL: ' + profile.getImageUrl());
-        // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
         
-        // console.log(profile)
         
-        return(profile.getId()) 
+        let MainContainer = document.getElementById("main")
+        MainContainer.classList.add("hide")
+        
         
 }
-
-console.log(onSignIn())
