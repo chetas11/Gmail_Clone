f99571a2ff56cae582b81b23d3fd9026a90a03c0
chetas11 <cbirmole@gmail.com>
2020-10-31 14:03:26 +0530

    SignIn button

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+        const DetailsDiv = document.getElementById("Details");
+        
+        
+        function onSignIn(googleUser) {
+                location.href = "inbox.html";
+                var profile = googleUser.getBasicProfile();
+                DetailsDiv.innerText = profile;
+                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                console.log('Name: ' + profile.getName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        }
