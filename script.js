2dee0cd377296fa09e9088069a11d4d31185872a
chetas11 <cbirmole@gmail.com>
2020-10-31 14:12:29 +0530

    SignIn button

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-
-        // let Detailspara = document.getElementById("Details");
-        
-        
+          
         function onSignIn(googleUser) {
-                location.href = "inbox.html";
                 var profile = googleUser.getBasicProfile();
                 console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
                 console.log('Name: ' + profile.getName());
