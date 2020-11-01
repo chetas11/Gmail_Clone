a18dddab302fc78d21ad34847fc374a1cbfb393a
chetas11 <cbirmole@gmail.com>
2020-11-01 11:19:48 +0530

    complete

--- script.js
+++ script.js
@@ -117,7 +117,7 @@
         })
 
         Searchbar.addEventListener("keyup", function(event) {
-            if (event.keyCode === 13) {
+            if (event.code === "Enter") {
                 event.preventDefault();
                 SearchMessages();
             }
