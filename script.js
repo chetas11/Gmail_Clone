a44023c9db0131d29424ba252ff89ddd336f66d7
chetas11 <cbirmole@gmail.com>
2020-11-01 07:54:04 +0530

    Design UI

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
 
         function CreateMail(msg){
             let SingleMail = document.createElement("p");
-            SingleMail.innerText = String(msg).substring(0.100)
+            SingleMail.innerText = String(msg).substring(0.50);
             Inbox.appendChild(SingleMail)
         }
 
