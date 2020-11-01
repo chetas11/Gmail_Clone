82236cdb16584835f361ece43844ba7d465dc846
chetas11 <cbirmole@gmail.com>
2020-11-01 08:29:39 +0530

    Design UI

--- script.js
+++ script.js
@@ -124,6 +124,23 @@
         })
 
 
+        function sendEmail()
+            {
+            $('#send-button').addClass('disabled');
+
+            sendMessage(
+                {
+                'To': $('#compose-to').val(),
+                'Subject': $('#compose-subject').val()
+                },
+                $('#compose-message').val(),
+                composeTidy
+            );
+
+            return false;
+            }
+
+
 
         authenticate().then(loadClient)
 
