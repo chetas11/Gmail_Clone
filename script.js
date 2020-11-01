e85501308d53655c0dfe531c6831737187ebd6f2
chetas11 <cbirmole@gmail.com>
2020-11-01 08:33:50 +0530

    Design UI

--- script.js
+++ script.js
@@ -140,6 +140,37 @@
             return false;
             }
 
+            function sendMessage(headers_obj, message, callback)
+            {
+            var email = '';
+
+            for(var header in headers_obj)
+                email += header += ": "+headers_obj[header]+"\r\n";
+
+                email += "\r\n" + message;
+
+            var sendRequest = gapi.client.gmail.users.messages.send({
+                'userId': Id,
+                'resource': {
+                'raw': window.btoa(email).replace(/\+/g, '-').replace(/\//g, '_')
+                }
+            });
+
+            return sendRequest.execute(callback);
+            }
+
+
+            function composeTidy()
+                {
+                $('#compose-modal').modal('hide');
+
+                $('#compose-to').val('');
+                $('#compose-subject').val('');
+                $('#compose-message').val('');
+
+                $('#send-button').removeClass('disabled');
+                }
+
 
 
         authenticate().then(loadClient)
